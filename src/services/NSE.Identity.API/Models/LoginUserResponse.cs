16cff7610ea5d9f24115530a89d5126d1c305883
Gustavo Miranda <gmiranda90@outlook.com>
2024-02-15 22:53:25 -0300

    Add Jwt token generation

---
+++ src/services/NSE.Identity.API/Models/LoginUserResponse.cs
@@ -0,0 +1,8 @@
+namespace NSE.Identity.API.Models;
+
+public class LoginUserResponse
+{
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UserToken UserToken { get; set; }
+}
